20fc199bf60bf5034148d58562040cc41de09e05
Myunggeun Park <myunggeunp@gmail.com>
2023-05-12 04:47:35 -0700

    Updated dashboard

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              <Route path="/" element={<Dashboard />} />
+              <Route path="/cyberdawgs-dashboard" element={<Dashboard />} />
               <Route path="/iBDawgsDashboard" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/features" element={<Features />} />
