230355763b70e2b1e24bd52e40a86c8879ee4200
jbryanp <jprase01@uw.edu>
2023-05-28 21:06:32 -0700

    added team picture

--- src/scenes/home/index.jsx
+++ src/scenes/home/index.jsx
@@ -1,11 +1,8 @@
 import { Box, useTheme } from "@mui/material";
 import Header from "../../components/Header";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { tokens } from "../../theme";
+import img1 from "../../images/members.png";
 
 const Home = () => {
   const theme = useTheme();
@@ -27,8 +24,17 @@ const Home = () => {
           >
             Our Team
           </Typography>
-          <Typography style={{ "margin-left": "10px", "margin-top": "10px" }}>
-            iBDawgs is a team of skilled software developers and cybersecurity analysts dedicated to making the digital world a safer place. At iBDawgs, we understand the critical importance of cybersecurity in today's digital landscape. Malicious cyber attacks can have devastating consequences for both individuals and organizations, and we are committed to developing innovative solutions that can help prevent these attacks from happening. With a focus on machine learning, iBDawgs is working to develop a powerful machine learning model to combat various malicious cyber-attacks.          </Typography>
+          <Typography style={{ "marginLeft": "10px", "marginTop": "10px" }}>
+            iBDawgs is a team of skilled software developers and cybersecurity analysts dedicated to making the digital world a safer place. At iBDawgs, we understand the critical importance of cybersecurity in today's digital landscape. Malicious cyber attacks can have devastating consequences for both individuals and organizations, and we are committed to developing innovative solutions that can help prevent these attacks from happening. With a focus on machine learning, iBDawgs is working to develop a powerful machine learning model to combat various malicious cyber-attacks.          
+          </Typography>
+          <Box display="flex" justifyContent="center" alignItems="center" marginTop={5}>
+                <img
+                  alt="team-photo"
+                  width="500"
+                  height="auto"
+                  src={img1}
+                />
+          </Box>
         </Box>
       </Box>
     </Box>
